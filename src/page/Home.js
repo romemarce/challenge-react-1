7c013deaf4d8fe1bb35978522278f9c6aabd0351
Marcelo Romero <romemarce1@gmail.com>
2022-08-15 10:49:48 -0300

    add image logo

--- src/page/Home.js
+++ src/page/Home.js
@@ -1,6 +1,8 @@
+import Logo from './../assets/img/logo.svg'
 const Home = () => {
   return (
   <section>
+    <img src={Logo} alt="logo" />
     <h1 className="title">Challenge React - 1</h1>
   </section>
   );
